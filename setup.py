d7ceefe2722a257434eed7a022b5d4a451c9b478
David Cofer <david.cofer@dynetics.com>
2018-10-10 10:10:16 -0500

    Created a setup script for the python file and moved the geoid.py into a package.

---
+++ setup.py
@@ -0,0 +1,15 @@
+__author__="Kim Vandry"
+__date__="07-August-2015 12:00:00 PM"
+
+from distutils.core import setup
+
+setup (
+	name = 'geoidheight',
+	version = '1.0.0',
+	description = 'Geoid height library',
+	#auth = 'Kim Vandry',
+	#author_email = 'vandry@TZoNE.ORG',
+	url = 'https://github.com/vandry/geoidheight',
+	packages = ['geoidheight',],
+	license = 'Lesser GNU Library General Public License'
+)
